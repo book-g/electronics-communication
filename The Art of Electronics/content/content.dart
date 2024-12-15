7e171ab89c00e8815c4e8461135450b4b5832af2
gobalkrishnan <gobalkrishnan.books@gmail.com>
2024-12-15 10:01:28 +0530

    the art of electronics - basic content

---
+++ The Art of Electronics/content/content.dart
@@ -0,0 +1,86 @@
+var the_art_of_the_electronics = {
+  "The Art Of Electronics": [
+    {
+      "book_authors": [
+        {
+          "author": [
+            {
+              "name": "Paul Horowitz",
+              "university": "Harvard University",
+              "image": "",
+            },
+            {
+              "name": "Winfield Hill",
+              "university": "Rowland Institute at Harvard",
+              "image": "",
+            }
+          ]
+        },
+        {"edition": "Third Edition"},
+        {"image": "//book_cover_image//"}
+      ]
+    },
+    {"List of Tables": []},
+    {"Preface to the First Edition": []},
+    {"Preface to the Second Edition": []},
+    {"Preface to the Third Edition": []},
+    {
+      "1. Foundations": [
+        {"1. Introduction": []},
+        {
+          "2. Voltage, current, and resistance": [
+            {
+              "1. Voltage and current": [],
+              "2. Relationship between voltage and current: resistors": [],
+              "3. Voltage dividers": [],
+              "4. Voltage sources and current sources": [],
+              "5. Thevenin equivalent circuit": [],
+              "6. Small-signal resistance": [],
+              "7. An example: 'It's too hot!": []
+            }
+          ]
+        },
+        {"3. Signals": []},
+        {"4. Capacitors and ac circuits": []},
+        {"5. Inductors and transformers": []},
+        {"6. Diodes and diode circuits": []},
+        {"7. Impedance and reactance": []},
+        {"8. Putting it all together - an AM radio": []},
+        {"9. Other passive components": []},
+        {
+          "10. A parting shot : confusing markings and itty-bitty components":
+              []
+        },
+      ]
+    },
+    {"2. Bipolar Transistors": []},
+    {"3. Field-Effect Transistors": []},
+    {"4. Operational Amplifiers": []},
+    {"5. Precision Circuits": []},
+    {"6. Filters": []},
+    {"7. Oscillators and Timers": []},
+    {"8. Low-Noise Techniques": []},
+    {"9. Voltage Regulation and Power Conversion": []},
+    {"10. Digital Logic": []},
+    {"11. Programmable Logic Devices": []},
+    {"12. Logic Interfacing": []},
+    {"13. Digital meets Analog": []},
+    {"14. Computers, Controllers, and Data Links": []},
+    {"15. Microcontrollers": []},
+    {"APPENDIX A: Math Review": []},
+    {"APPENDIX B: How to Draw Schematic Diagrams": []},
+    {"APPENDIX C: Resistor Types": []},
+    {"APPENDIX D: Thevenin's Theorem": []},
+    {"APPENDIX E: LC Butterworth Filters": []},
+    {"APPENDIX F: Load Lines": []},
+    {"APPENDIX G: The Curve Tracer": []},
+    {"APPENDIX H: Transmission Lines and Impedance Matching": []},
+    {"APPENDIX I: Television: A Compact Tutorial": []},
+    {"APPENDIX J: SPICE Primer": []},
+    {"APPENDIX K: 'Where Do I Go to Buy Electronic Goodies?'": []},
+    {"APPENDIX L: Workbench Instruments and Tools": []},
+    {"APPENDIX M: Catalogs, Magazines, Databooks": []},
+    {"APPENDIX N: Further Reading and References": []},
+    {"APPENDIX O: The Oscilloscope": []}
+  ],
+};
